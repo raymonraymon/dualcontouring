fa193a7dc62ce857f2529e39c12ecbb5cce436f7
raymonraymon <raymond03152410@163.com>
2021-09-01 17:46:43 +0800

    test new key

--- dc.cpp
+++ dc.cpp
@@ -24,7 +24,7 @@
 #include <iostream>
 
 #include <boost/program_options.hpp>
-namespace po = boost::program_options;
+//namespace po = boost::program_options;
 
 //#define ALLOW_INTERSECTION
 
@@ -37,47 +37,63 @@ namespace po = boost::program_options;
 
 int main( int args, char* argv[] )
 {
-	// Declare the supported options.
-	po::options_description desc("Allowed options");
-	desc.add_options()
-		("help", "produce help message")
-		("simplify", po::value<float>(), "set simplify threshold (float)")
-		("nointer", "use intersection-free algorithm")
-		("test", "run intersection test")
-	;
+	//// Declare the supported options.
+	//po::options_description desc("Allowed options");
+	//desc.add_options()
+	//	("help", "produce help message")
+	//	("simplify", po::value<float>(), "set simplify threshold (float)")
+	//	("nointer", "use intersection-free algorithm")
+	//	("test", "run intersection test")
+	//;
 
-	po::variables_map vm;
-	po::store(po::parse_command_line(args, argv, desc), vm);
-	po::notify(vm);    
+	//po::variables_map vm;
+	//po::store(po::parse_command_line(args, argv, desc), vm);
+	//po::notify(vm);    
 
-	if (vm.count("help")) {
-		std::cout << desc << "\n";
-		return 1;
-	}
-	float simplify_threshold = -1;
-	if (vm.count("simplify")) {
-		simplify_threshold = vm["simplify"].as<float>();
-		std::cout << "Simplify threshold set: " << simplify_threshold << "\n";
-	} else {
-		std::cout << "Simplify not set.\n";
-	}
+	//if (vm.count("help")) {
+	//	std::cout << desc << "\n";
+	//	return 1;
+	//}
+	//float simplify_threshold = -1;
+	//if (vm.count("simplify")) {
+	//	simplify_threshold = vm["simplify"].as<float>();
+	//	std::cout << "Simplify threshold set: " << simplify_threshold << "\n";
+	//} else {
+	//	std::cout << "Simplify not set.\n";
+	//}
 
+	//// Read input file
+	//std::cout << " input file: " << argv[1] << "\n";
+	//Octree* mytree = new Octree( argv[1], simplify_threshold ) ;
+
+	//if (vm.count("nointer")) {
+	//	std::cout << "Intersection-free algorithm! [Ju et al. 2006] \n";
+	//	mytree->genContourNoInter2( argv[2] ) ;
+	//} else {
+	//	std::cout << "Original algorithm! [Ju et al. 2002] \n";
+	//	mytree->genContour( argv[2] ) ;
+	//}
+	//
+	//if (vm.count("test")) {
+	//	printf("Running intersection test... \n") ;
+	//	int num = Intersection::testIntersection( argv[2], argv[3] ); // Pairwise intersection test - may take a while
+	//	printf("%d intersections found!\n", num) ;
+	//}
+
+
+
+	float simplify_threshold = 0.1; 
 	// Read input file
 	std::cout << " input file: " << argv[1] << "\n";
-	Octree* mytree = new Octree( argv[1], simplify_threshold ) ;
-
-	if (vm.count("nointer")) {
+	Octree* mytree = new Octree(argv[1], simplify_threshold);
+	if (0) {
 		std::cout << "Intersection-free algorithm! [Ju et al. 2006] \n";
 		mytree->genContourNoInter2( argv[2] ) ;
-	} else {
-		std::cout << "Original algorithm! [Ju et al. 2002] \n";
-		mytree->genContour( argv[2] ) ;
 	}
-	
-	if (vm.count("test")) {
-		printf("Running intersection test... \n") ;
-		int num = Intersection::testIntersection( argv[2], argv[3] ); // Pairwise intersection test - may take a while
-		printf("%d intersections found!\n", num) ;
+	else {
+		std::cout << "Original algorithm! [Ju et al. 2002] \n";
+		mytree->genContour(argv[2]);
+
 	}
 }
 
